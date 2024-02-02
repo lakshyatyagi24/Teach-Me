fa844e0bc8fd32a288d304c5ea702e01e9582608
IlanVinerski <ilanv145@gmail.com>
2024-02-02 16:29:10 +0200

    Add Loader and Message components

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -3,6 +3,8 @@ import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Row, Col } from "react-bootstrap";
 import Course from "../components/Course";
+import Message from "../components/Message";
+import Loader from "../components/Loader";
 import { listCourses } from "../actions/courseActions";
 
 const HomeScreen = () => {
@@ -20,14 +22,19 @@ const HomeScreen = () => {
   return (
     <>
       <h1>Our Courses</h1>
-      {loading ? <h2>Loading...</h2> : error ? <h3>{error}</h3> : <Row>
-        {courses.map((course) => (
-          <Col key={course._id} sm={12} md={6} lg={4} xl={3}>
-            <Course course={course} />
-          </Col>
-        ))}
-      </Row>}
-      
+      {loading ? (
+        <Loader />
+      ) : error ? (
+        <Message variant='danger'>{error}</Message>
+      ) : (
+        <Row>
+          {courses.map((course) => (
+            <Col key={course._id} sm={12} md={6} lg={4} xl={3}>
+              <Course course={course} />
+            </Col>
+          ))}
+        </Row>
+      )}
     </>
   );
 };
