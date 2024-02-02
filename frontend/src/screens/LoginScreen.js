490d392249ca42d591a47f8c6c82fd1dafe06ee2
IlanVinerski <ilanv145@gmail.com>
2024-02-02 21:04:19 +0200

    Update LoginScreen.js

--- frontend/src/screens/LoginScreen.js
+++ frontend/src/screens/LoginScreen.js
@@ -1,18 +1,43 @@
-import React, { useState, useEffect } from 'react';
-import {Link} from 'react-router-dom'
-import {Row, Col, Button, Form} from 'react-bootstrap'
-import {useSelector,useDispatch} from 'react-redux'
-import Message from '../components/Message'
-import Loader from '../components/Loader'
-import FormContainer from '../components/FormContainer'
-import {login} from '../actions/userActions'
-
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import { Row, Col, Button, Form } from "react-bootstrap";
+import { useSelector, useDispatch } from "react-redux";
+import Message from "../components/Message";
+import Loader from "../components/Loader";
+import FormContainer from "../components/FormContainer";
+import { login } from "../actions/userActions";
 
 const LoginScreen = () => {
-    
-    return(
-        <h1>Sign In</h1>
-    )
-}
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-export default LoginScreen
+  return (
+    <FormContainer>
+      <h1>Sign In</h1>
+      <Form onSubmit={submitHandler}>
+        <Form.Group controlId="email">
+          <Form.Label>Email Address</Form.Label>
+          <Form.Control
+            type="email"
+            placeholder="Enter email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          ></Form.Control>
+        </Form.Group>
+
+        <Form.Group controlId="password">
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            type="password"
+            placeholder="Enter password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          ></Form.Control>
+        </Form.Group>
+
+      </Form>
+    </FormContainer>
+  );
+};
+
+export default LoginScreen;
