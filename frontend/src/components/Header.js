4f29e98e084b303ee8596bb7d573057f865b0ade
Damti Orel <oreldamti1996@gmail.com>
2024-02-05 19:37:51 +0200

    Show User In Navbar & Logout

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,9 +1,20 @@
 // eslint-disable-next-line
 import React from "react";
+import {useDispatch, useSelector} from 'react-redux'
 import { LinkContainer } from "react-router-bootstrap";
-import { Container, Nav, Navbar } from "react-bootstrap";
+import { Container, Nav, Navbar , NavDropdown} from "react-bootstrap";
+import {logout} from '../actions/userActions';
 
 const Header = () => {
+  const dispatch = useDispatch()
+
+  const userLogin = useSelector((state)=> state.userLogin)
+  const {userInfo} = userLogin
+
+  const logoutHandler = () =>{
+    dispatch(logout())
+  }
+
   return (
     <header>
       <Navbar bg="dark" data-bs-theme="dark" variant="dark" collapseOnSelect>
@@ -14,11 +25,18 @@ const Header = () => {
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ms-auto">
-              <LinkContainer to="/login">
-                <Nav.Link>
-                  <i className="fa-solid fa-user"></i> Sign In
-                </Nav.Link>
-              </LinkContainer>
+              {userInfo ? (
+                <NavDropdown title = {userInfo.name} id ='username'>
+                  <LinkContainer to='/profile'>
+                    <NavDropdown.Item>Profile</NavDropdown.Item>
+                  </LinkContainer>
+                  <NavDropdown.Item onClick={logoutHandler}>Logout</NavDropdown.Item>
+                </NavDropdown>
+              ): <LinkContainer to="/login">
+              <Nav.Link>
+                <i className="fa-solid fa-user"></i> Sign In
+              </Nav.Link>
+            </LinkContainer>}
             </Nav>
           </Navbar.Collapse>
         </Container>
