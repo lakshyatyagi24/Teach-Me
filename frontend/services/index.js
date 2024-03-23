d544ed62a97dfd909419f668e7ab9a4b3529d24f
Damti Orel <oreldamti1996@gmail.com>
2024-03-23 22:14:48 +0200

    Fix the access to url to backend

--- frontend/services/index.js
+++ frontend/services/index.js
@@ -2,6 +2,6 @@ import axios from "axios";
 
 export const axiosApi = () => {
   axios.create({
-    baseURL: `${REACT_APP_BACKEND_URL}/api/`,
+    baseURL: `${process.env.REACT_APP_BACKEND_URL}/api/`,
   });
 };
